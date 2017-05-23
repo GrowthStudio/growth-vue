f48c62d32b2c44fb0ac3554177be004204ca5a94
Phodal Huang <h@phodal.com>
2017-05-23 08:48:28 +0800

    [T] CI: fix e2e test

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,8 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
+      .assert.elementPresent('.timeline-demo')
+      .assert.containsText('p', 'This is home wrapper')
       .assert.elementCount('img', 1)
       .end()
   }
